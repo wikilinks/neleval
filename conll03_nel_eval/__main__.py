8962d786fa69f5518fa671f95e785e620e083965
Will Radford <wejradford@gmail.com>
2014-02-11 22:18:21 +1100

    Tweaked command ordering. Began mapping work.

--- conll03_nel_eval/__main__.py
+++ conll03_nel_eval/__main__.py
@@ -11,7 +11,6 @@ APPS = [
 
 def main(args=sys.argv[1:]):
     p = argparse.ArgumentParser(description='Evaluation tools for Named Entity Linking output.')
-    p.add_argument('fname', metavar='FILE')
     sp = p.add_subparsers()
     for a in APPS:
         a.add_arguments(sp)
--- conll03_nel_eval/evaluate.py
+++ conll03_nel_eval/evaluate.py
@@ -30,6 +30,7 @@ class Evaluate(object):
     @classmethod
     def add_arguments(cls, sp):
         p = sp.add_parser('evaluate', help='Evaluate system output')
+        p.add_argument('fname', metavar='FILE')
         p.add_argument('-g', '--gold')
         p.set_defaults(cls=cls)
         return p
