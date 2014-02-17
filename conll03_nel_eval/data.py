3a71d8cca79b6d98e55bd5e6a5ba2636eee7f8d9
Will Radford <wejradford@gmail.com>
2014-02-17 22:54:15 +1100

    Partial implementation of new data model.

--- conll03_nel_eval/data.py
+++ conll03_nel_eval/data.py
@@ -3,6 +3,7 @@
 Container for CoNLL03 NEL annotation
 """
 import gzip
+import re
 from collections import OrderedDict
 
 MATCHES = '''
@@ -17,7 +18,160 @@ strong_all_match
 weak_all_match
 '''.strip().split()
 
+TEMPLATE = u'{}\t{}\t{}\t{}'
+ENC = 'utf8'
+
+# Different styles of format.
+class CoNLLDialect(object):
+    DOC_ID = re.compile('^-DOCSTART- \((?<doc_id>.*)\)$')
+    NIL = '--NME--'
+    def __init__(self):
+        pass
+
+    def extract_doc_id(self, line):
+        m = self.DOC_ID.match(line)
+        if m is not None:
+            # TODO Splits?
+            return m.group(1)
+    
+    def format_doc_id(self, doc_id):
+        return '-DOCSTART- ({})'.format(doc_id)
+
+    def extract_link(self, line):
+        line_bits = line.split('\t')
+        token = line_bits.pop(0)
+        iob = None
+        name = None
+        link = None
+        score = None
+        if line_bits:
+            assert len(line_bits) >= 2:
+            # [iob, name, wikipedia_id, wikipedia_url, numeric_id, freebase_id]
+            if len(line_bits) == 6:
+                iob = line_bits[0]
+                name = line_bits[1]
+                # FIXME Configurable?
+                link = line_bits[2]
+            # [iob, name, entity_id, score]
+            elif len(line_bits) == 4:
+                iob = line_bits[0]
+                name = line_bits[1]
+                link = line_bits[2]
+                score = float(line_bits[3])
+            # [iob, name, entity_id]
+            elif len(line_bits) == 3:
+                iob = line_bits[0]
+                name = line_bits[1]
+                link = line_bits[2]
+            # [iob, name] - this may be non-standard...
+            elif len(line_bits) == 2:
+                iob = line_bits[0]
+                name = line_bits[1]
+            else:
+                assert False, 'Impossible choice {}'.format(line_bits)
+        if link == self.NIL:
+            link = None
+        return token, iob, name, link, score
+
+DIALECTS = {
+    'CoNLL': CoNLLDialect,
+}
+DEFAULT_DIALECT = 'CoNLL'
+
+# Data model.
+class Span(object):
+    __slots__ = ['start', 'end']
+    def __init__(self, start, end):
+        assert isinstance(start, int) and 0 <= start
+        assert isinstance(end, int) and start < end
+        self.start = start
+        self.end = end
+
+class Token(Span):
+    __slots__ = ['text', 'start', 'end']
+    def __init__(self, start, end, text):
+        assert isinstance(text, unicode)
+        super(Token, self).__init__(start, end)
+        self.text = text
+
+    def __str__(self):
+        return TEMPLATE.format(self.text).encode(ENC)
+
 class Mention(object):
+    """ A mention composed of 1+ tokens, possibly links. """
+    __slots__ = ['texts', 'link', 'score']
+    def __init__(self, start, end, texts, link=None, score=None):
+        assert isinstance(texts, list) and all(isinstance(unicode, i) for i in texts)
+        assert link is None or isinstance(link, unicode)
+        super(Mention, self).__init__(start, end)
+        self.texts = texts
+        self.link = link
+        self.score = score
+
+    def __str__(self):
+        lines = []
+        link = self.link or ''
+        score = self.score or ''
+        for i, t in enumerate(self.texts):
+            if i == 0:
+                iob = 'B'
+            else:
+                iob = 'I'
+            lines.append(TEMPLATE.format(t, iob, link, score))
+        return u'\n'.join(lines).encode(ENC)
+
+class Sentence(object):
+    def __init__(self, spans):
+        assert spans and all(isinstance(Span, s) for s in spans)
+        self.spans = spans
+
+    def __str__(self):
+        return u'\n'.join(unicode(s) for s in self.spans)
+
+class Document(object):
+    def __init__(self, doc_id, sentences, dialect):
+        self.doc_id = doc_id
+        self.sentences = sentences
+        self.dialect = dialect
+
+    def __str__(self):
+        return u'{}{}\n'.format(self.doc_id, u'\n'.join(unicode(s) for s in self.sentences)).encode(ENC)
+
+class Parser(object):
+    def __init__(self, dialect_name=DEFAULT_DIALECT):
+        assert dialect_name in DIALECTS, 'Invalid dialect "{}"'.format(dialect_name)
+        self.dialect = DIALECTS[dialect_name]
+
+    def parse(self, f):
+        ''' Yields Documents. '''
+        doc_id = None
+        sentences = []
+        lines = (enumerate(l.strip() for l in f))
+        try:
+            i, l = lines.next()
+            temp_doc_id = self.dialect.extract_doc_id(l)
+            if temp_doc_id:
+                yield self.build_doc(doc_id, sentences)
+                doc_id = temp_doc_id
+                sentences = []
+            # Grab the sentence tokens.
+            else:
+                j = 0 # The token index in the sentence.
+                while l:
+                    token, iob, name, link, score = self.dialect.extract_link(l)
+                    # TODO Pack tokens into mentions.
+                    i, l = lines.next()
+                    j += 1
+        except StopIteration:
+            # Handle last document.
+            yield self.build_doc(doc_id, sentences)
+    
+    def build_doc(self, doc_id, sentences):
+        return Doc(doc_id, sentences, dialect=self.dialect)
+        
+# FIXME Let's keep matchers out of the data model.
+
+class OldMention(object):
     """Named entity mention."""
     def __init__(self, start, end, link=None):
         """
@@ -38,7 +192,7 @@ class Mention(object):
             return self.link[self.link.rfind('/')+1:].replace('_', ' ')
 
     def __str__(self):
-        return '<Mention [{}:{}] -> {}>'.format(self.start, self.end, self.link)
+        return '<OldMention [{}:{}] -> {}>'.format(self.start, self.end, self.link)
 
     def __cmp__(self, other):
         return cmp(self.start, other.start) or cmp(self.end, other.end)
@@ -62,14 +216,14 @@ class Mention(object):
     def weak_link_match(self, other):
         return self.weak_span_match(other) and self.link_match(other)
 
-class Document(object):
+class OldDocument(object):
     def __init__(self, id, mentions=None, lines=None):
         self.id, self.split = self._parse_id(id)
         self.mentions = mentions or []
         self.lines = lines or [] # FIXME Temporary inclusion of lines until tokens.
 
     def __str__(self):
-        return '<Document id={}>'.format(self.id)
+        return '<OldDocument id={}>'.format(self.id)
 
     @property
     def links(self):
@@ -182,7 +336,7 @@ class Document(object):
                 continue # blank line
             tok, bi, name, link = cls._parse_line(line)
             if start is not None and bi != 'I':
-                yield Mention(start, tok_id+1, prev)
+                yield OldMention(start, tok_id+1, prev)
                 start = None
             tok_id += 1
             prev = link
@@ -191,7 +345,7 @@ class Document(object):
             if bi == 'B':
                 start = tok_id
         if start is not None:
-            yield Mention(start, tok_id+1, prev)
+            yield OldMention(start, tok_id+1, prev)
 
     NIL_LINK = '--NME--'
     @classmethod
@@ -212,7 +366,7 @@ class Document(object):
     def to_conll(self, mapping=None):
         ''' Returns a CoNLL formatted string. '''
         if mapping:
-            pass #TODO Actually tweak here, requires proper modelling of Mentions.
+            pass #TODO Actually tweak here, requires proper modelling of OldMentions.
         return ''.join(self.lines)
 
 
@@ -242,11 +396,11 @@ class Data(object):
         for i, line in enumerate(lines):
             if line.startswith('-DOCSTART-'):
                 if len(doc) > 0:
-                    yield Document.from_lines(doc)
+                    yield OldDocument.from_lines(doc)
                 doc = []
             if line.strip() != '':
                 doc.append(line)
-        yield Document.from_lines(doc)
+        yield OldDocument.from_lines(doc)
 
     def __iter__(self):
         return iter(self.documents.values())
