f1925bc8dd1540e5764fd9a00256d9af552090ab
Will Radford <wejradford@gmail.com>
2014-02-24 22:07:56 +1100

    Refactored data to model Documents, Sentences, Mentions and Tokens (the latter two Spans). Evaluation uses a hashing technique and should be faster. Mappings now use underscores, as per original AIDA data. Removed two nil evaluations.

--- conll03_nel_eval/test.py
+++ conll03_nel_eval/test.py
@@ -2,21 +2,22 @@
 import os
 from pprint import pprint
 
-from data import Data
+from data import Reader
 from evaluate import Evaluate
 
 DIR = os.path.join(os.path.dirname(__file__))
 EXAMPLES = os.path.join(DIR, 'examples')
 
 DATA = os.path.join(EXAMPLES, 'data.txt')
+DATA_FULL = os.path.join(EXAMPLES, 'data_full.txt')
 
 def test_data():
-    d = Data.read(DATA)
+    d = list(Reader(open(DATA)))
     assert len(d) == 1
     doc = list(d)[0]
-    assert len(doc.mentions) == 2
-    assert len(doc.links) == 1
-    assert len(doc.nils) == 1
+    assert len(list(doc.sentences[0].iter_mentions())) == 2
+    assert len(list(doc.sentences[0].iter_links())) == 1
+    assert len(list(doc.sentences[0].iter_nils())) == 1
 
 def _get_stats(gold_fname, sys_fname):
     gold_path = os.path.join(EXAMPLES, gold_fname)
@@ -24,6 +25,12 @@ def _get_stats(gold_fname, sys_fname):
     stats = Evaluate(sys_path, gold=gold_path)()
     pprint(stats)
     return stats
+
+def check_correct(expected, actual):
+    assert expected.viewkeys() == actual.viewkeys(), 'Different keys\nexpected\t{}\nactual\t{}'.format(sorted(expected.keys()), sorted(actual.keys()))
+    for k in expected:
+        assert expected[k] == actual[k], 'Different on key "{}".\nexpected\t{}\nactual\t{}'.format(k, expected[k], actual[k])
+    return True
 
 CORRECT = {
  'link_entity_match': {'fn': 0,
@@ -50,12 +57,6 @@ CORRECT = {
                           'precision': 1.0,
                           'recall': 1.0,
                           'tp': 2},
- 'strong_nil_match': {'fn': 0,
-                      'fp': 0,
-                      'fscore': 1.0,
-                      'precision': 1.0,
-                      'recall': 1.0,
-                      'tp': 1},
  'weak_all_match': {'fn': 0,
                     'fp': 0,
                     'fscore': 1.0,
@@ -74,15 +75,10 @@ CORRECT = {
                         'precision': 1.0,
                         'recall': 1.0,
                         'tp': 2},
- 'weak_nil_match': {'fn': 0,
-                    'fp': 0,
-                    'fscore': 1.0,
-                    'precision': 1.0,
-                    'recall': 1.0,
-                    'tp': 1}}
+}
 
 def test_correct():
-    assert CORRECT == _get_stats('data.txt', 'data.txt')
+    assert check_correct(CORRECT, _get_stats('data.txt', 'data.txt'))
 
 ATTEMPT = {
  'link_entity_match': {'fn': 0,
@@ -109,12 +105,6 @@ ATTEMPT = {
                           'precision': 1.0,
                           'recall': 1.0,
                           'tp': 2},
- 'strong_nil_match': {'fn': 1,
-                      'fp': 0,
-                      'fscore': 0.0,
-                      'precision': 1.0,
-                      'recall': 0.0,
-                      'tp': 0},
  'weak_all_match': {'fn': 1,
                     'fp': 1,
                     'fscore': 0.5,
@@ -133,16 +123,10 @@ ATTEMPT = {
                         'precision': 1.0,
                         'recall': 1.0,
                         'tp': 2},
- 'weak_nil_match': {'fn': 1,
-                    'fp': 0,
-                    'fscore': 0.0,
-                    'precision': 1.0,
-                    'recall': 0.0,
-                    'tp': 0},
 }
 
 def test_attempt():
-    assert ATTEMPT == _get_stats('data.txt', 'data_attempt.txt')
+    assert check_correct(ATTEMPT, _get_stats('data.txt', 'data_attempt.txt'))
 
 CORRECT_MORE = {
  'link_entity_match': {'fn': 0,
@@ -169,12 +153,6 @@ CORRECT_MORE = {
                           'precision': 1.0,
                           'recall': 1.0,
                           'tp': 4},
- 'strong_nil_match': {'fn': 0,
-                      'fp': 0,
-                      'fscore': 1.0,
-                      'precision': 1.0,
-                      'recall': 1.0,
-                      'tp': 1},
  'weak_all_match': {'fn': 0,
                     'fp': 0,
                     'fscore': 1.0,
@@ -193,15 +171,10 @@ CORRECT_MORE = {
                         'precision': 1.0,
                         'recall': 1.0,
                         'tp': 4},
- 'weak_nil_match': {'fn': 0,
-                    'fp': 0,
-                    'fscore': 1.0,
-                    'precision': 1.0,
-                    'recall': 1.0,
-                    'tp': 1}}
+}
 
 def test_more_correct():
-    assert CORRECT_MORE == _get_stats('data_more.txt', 'data_more.txt')
+    assert check_correct(CORRECT_MORE, _get_stats('data_more.txt', 'data_more.txt'))
 
 CORRECT_ATTEMPT = {
  'link_entity_match': {'fn': 2,
@@ -228,12 +201,6 @@ CORRECT_ATTEMPT = {
                           'precision': 1.0,
                           'recall': 1.0,
                           'tp': 4},
- 'strong_nil_match': {'fn': 1,
-                      'fp': 1,
-                      'fscore': 1.0,
-                      'precision': 0.0,
-                      'recall': 0.0,
-                      'tp': 0},
  'weak_all_match': {'fn': 3,
                     'fp': 3,
                     'fscore': 0.25,
@@ -252,12 +219,7 @@ CORRECT_ATTEMPT = {
                         'precision': 1.0,
                         'recall': 1.0,
                         'tp': 4},
- 'weak_nil_match': {'fn': 1,
-                    'fp': 1,
-                    'fscore': 1.0,
-                    'precision': 0.0,
-                    'recall': 0.0,
-                    'tp': 0}}
+}
 
 def test_more_attempt():
-    assert CORRECT_ATTEMPT == _get_stats('data_more.txt', 'data_more_attempt.txt')
+    assert check_correct(CORRECT_ATTEMPT, _get_stats('data_more.txt', 'data_more_attempt.txt'))
