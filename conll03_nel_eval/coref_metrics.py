5070c884abe2dfde11a096edfdae28d40376d3e4
Joel Nothman <joel.nothman@gmail.com>
2014-05-13 13:56:59 +1000

    Other metrics

--- conll03_nel_eval/coref_metrics.py
+++ conll03_nel_eval/coref_metrics.py
@@ -1,17 +1,44 @@
 from __future__ import division
 
 from functools import partial
+from collections import defaultdict
+import itertools
 
 import numpy as np
 
 from .munkres import linear_assignment
 
-# Clusters are most readily evaluated when represented as a
-# mapping from cluster ID to set of mention IDs. Could equally
-# use a set of frozensets, but lose debugging info.
+
+# TODO: Blanc and standard clustering metrics (e.g. http://scikit-learn.org/stable/modules/clustering.html)
+# TODO: cite originating papers
+# XXX: perhaps use set (or list) of sets rather than dict of sets
 
 
-def cluster_sim_f1(a, b):
+def mapping_to_sets(mapping):
+    """
+    >>> sets = mapping_to_sets({'a': 1, 'b': 2, 'c': 1}).items()
+    >>> sorted((k, sorted(v)) for k, v in sets)
+    [(1, ['a', 'c']), (2, ['b'])]
+    """
+    s = defaultdict(set)
+    for m, k in mapping.items():
+        s[k].add(m)
+    return dict(s)
+
+
+def sets_to_mapping(s):
+    """
+    >>> sorted(sets_to_mapping({1: {'a', 'c'}, 2: {'b'}}).items())
+    [('a', 1), ('b', 2), ('c', 1)]
+    """
+    return {m: k for k, ms in s.items() for m in ms}
+
+
+def _f1(a, b):
+    return 2 * a * b / (a + b)
+
+
+def dice(a, b):
     """
 
     "Entity-based" measure in CoNLL; #4 in CEAF paper
@@ -21,7 +48,7 @@ def cluster_sim_f1(a, b):
     return 0.
 
 
-def cluster_sim_overlap(a, b):
+def overlap(a, b):
     """Intersection of sets
 
     "Mention-based" measure in CoNLL; #3 in CEAF paper
@@ -29,7 +56,7 @@ def cluster_sim_overlap(a, b):
     return len(a & b)
 
 
-def ceaf(true, pred, similarity=cluster_sim_f1):
+def ceaf(true, pred, similarity=dice):
     """
 
     >>> true = {'A': {1,2,3,4,5}, 'B': {6,7}, 'C': {8, 9, 10, 11, 12}}
@@ -65,8 +92,94 @@ def ceaf(true, pred, similarity=cluster_sim_f1):
     pred_denom = sum(similarity(S, S) for S in pred)
     p = numerator / pred_denom
     r = numerator / true_denom
-    return p, r, 2 * p * r / (p + r)
+    return p, r, _f1(p, r)
 
 
-entity_ceaf = partial(ceaf, similarity=cluster_sim_f1)
-mention_ceaf = partial(ceaf, similarity=cluster_sim_overlap)
+entity_ceaf = partial(ceaf, similarity=dice)
+mention_ceaf = partial(ceaf, similarity=overlap)
+
+
+def _b_cubed(A, B, A_mapping, B_mapping, EMPTY=frozenset([])):
+    res = 0.
+    for m, k in A_mapping.items():
+        A_cluster = A.get(k, EMPTY)
+        res += len(A_cluster & B.get(B_mapping.get(m), EMPTY)) / len(A_cluster)
+    res /= len(A_mapping)
+    return res
+
+
+def b_cubed(true, pred):
+    """
+
+    TODO: tests
+    """
+    true_mapping = sets_to_mapping(true)
+    pred_mapping = sets_to_mapping(pred)
+    p = _b_cubed(pred, true, pred_mapping, true_mapping)
+    r = _b_cubed(true, pred, true_mapping, pred_mapping)
+    return p, r, _f1(p, r)
+
+
+def pairwise_f1(true, pred):
+    """Measure the proportion of correctly identified pairwise coindexations
+
+    This is called MUC score, and erroneously cited to Vilain et al. (2102) in
+    the CoNLL 2011-2012 Shared Task descriptions.
+
+    TODO: tests
+    """
+    pred_mapping = sets_to_mapping(pred)
+    correct = 0
+    for cluster in true.values():
+        for m1, m2 in itertools.combinations(cluster, 2):
+            if pred_mapping.get(m1) == pred_mapping.get(m2):
+                correct += 1
+    p = correct / sum(len(cluster) - 1 for cluster in pred.values())
+    r = correct / sum(len(cluster) - 1 for cluster in true.values())
+    return p, r, _f1(p, r)
+
+
+def _vilain(A, B_mapping):
+    numerator = 0
+    denominator = 0
+    for cluster in A.values():
+        corresponding = set()
+        n_unaligned = 0
+        for m in cluster:
+            if m not in B_mapping:
+                n_unaligned += 1
+            else:
+                corresponding.add(B_mapping[m])
+        numerator += len(cluster) - n_unaligned - len(corresponding)
+        denominator += len(cluster) - 1
+    return numerator / denominator
+
+
+def vilain(true, pred):
+    """The MUC evaluation metric defined in Vilain et al. (1995)
+
+    This calculates recall error for each true cluster C as the number of
+    response clusters that would need to be merged in order to produce a
+    superset of C.
+
+    Examples from Vilain et al. (1995):
+    >>> vilain({1: {'A', 'B', 'C', 'D'}},
+    ...        {1: {'A', 'B'}, 2: {'C', 'D'}})  # doctest: +ELLIPSIS
+    (1.0, 0.66..., 0.8)
+    >>> vilain({1: {'A', 'B'}, 2: {'C', 'D'}},
+    ...        {1: {'A', 'B', 'C', 'D'}})  # doctest: +ELLIPSIS
+    (0.66..., 1.0, 0.8)
+    >>> vilain({1: {'A', 'B', 'C'}}, {1: {'A', 'C'}})  # doctest: +ELLIPSIS
+    (1.0, 0.5, 0.66...)
+    >>> vilain({1: {'B', 'C', 'D', 'E', 'G', 'H', 'J'}},
+    ...        {1: {'A', 'B', 'C'}, 2: {'D', 'E', 'F'}, 3: {'G', 'H', 'I'}})
+    ... # doctest: +ELLIPSIS
+    (0.5, 0.5, 0.5)
+    >>> vilain({1: {'A', 'B', 'C'}, 2: {'D', 'E', 'F', 'G'}},
+    ...        {1: {'A', 'B'}, 2: {'C', 'D'}, 3: {'F', 'G', 'H'}})
+    ... # doctest: +ELLIPSIS
+    (0.5, 0.4, 0.44...)
+    """
+    p = _vilain(pred, sets_to_mapping(true))
+    r = _vilain(true, sets_to_mapping(pred))
+    return p, r, _f1(p, r)
