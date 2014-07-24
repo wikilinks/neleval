43213c72ddad2f0ffc330940b3863d9f7d3456dc
Ben Hachey <ben.hachey@gmail.com>
2014-07-24 10:25:51 +1000

    control script for tac13 evaluation and analysis.

---
+++ scripts/run_analysis.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Run analysis and save to file
+
+usage="Usage: $0 GOLD_XML GOLD_TAB SYSTEMS_DIR OUT_DIR"
+
+if [ "$#" -ne 2 ]; then
+    echo $usage
+    exit 1
+fi
+
+gold=$1; shift # prepared gold standard annotations (.combined.tsv)
+sys=$1; shift # prepared system annotations (.combined.tsv)
+
+out=`echo $sys | sed 's/.combined.tsv/.analysis/'`
+./cne analyze \
+    -s \
+    -g $gold \
+    $sys \
+    > $out
