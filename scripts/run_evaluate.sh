50a07324a1c40897298b9a73812fc73418690cde
Ben Hachey <ben.hachey@gmail.com>
2014-07-24 10:25:51 +1000

    control script for tac13 evaluation and analysis.

---
+++ scripts/run_evaluate.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Evaluate and save to file
+
+usage="Usage: $0 GOLD SYSTEM"
+
+if [ "$#" -ne 2 ]; then
+    echo $usage
+    exit 1
+fi
+
+gold=$1; shift # prepared gold standard annotations (.combined.tsv)
+sys=$1; shift # prepared system annotations (.combined.tsv)
+
+out=`echo $sys | sed 's/.combined.tsv/.evaluation/'`
+./cne evaluate \
+    -l tac \
+    -c tac \
+    -f 'tab_format' \
+    -g $gold \
+    $sys \
+    > $out
