9f642bf8502de858250863632764eb9690e6c5a5
ben.hachey <ben.hachey@gmail.com>
2014-08-08 22:10:20 +1000

    report mention_ceaf not entity_ceaf

--- scripts/run_tac14_report.sh
+++ scripts/run_tac14_report.sh
@@ -14,22 +14,22 @@ outdir=$1; shift # directory to which results are written
 
 # INITIALISE REPORT HEADER
 report=$outdir/00report.tab
-echo -e "WikiF1\tCEAFeP\tCEAFeR\tCEAFeF1\tSystem" \
+echo -e "WikiF1\tCEAFmP\tCEAFmR\tCEAFmF1\tSystem" \
     > $report
 
 # ADD SYSTEM SCORES
 for eval in $outdir/*.evaluation
 do
     cat $eval \
-	| grep 'strong_typed_all_match' \
-	| cut -f7 \
+        | grep -P '\tstrong_typed_all_match$' \
+        | cut -f7 \
         | tr '\n' '\t' \
-	>> $report
+        >> $report
     cat $eval \
-	| grep 'entity_ceaf' \
-	| cut -f5,6,7 \
+        | grep -P '\tmention_ceaf$' \
+        | cut -f5,6,7 \
         | tr '\n' '\t' \
-	>> $report
+        >> $report
     basename $eval \
         | sed 's/\.evaluation//' \
         >> $report
