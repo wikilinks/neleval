cefaf7f81bcc010395f0d95944147b111e63c709
Joel Nothman <joel.nothman@gmail.com>
2017-04-06 16:11:33 +1000

    Add first mention measures to run_evaluate

--- scripts/run_evaluate.sh
+++ scripts/run_evaluate.sh
@@ -15,7 +15,7 @@ sys=$1; shift # prepared system annotations (.combined.tsv)
 
 out=`echo $sys | sed 's/.combined.tsv/.evaluation/'`
 ./nel evaluate \
-    -m all \
+    -m all -m mention_ceaf:is_first:span -m b_cubed:is_first:span -m muc:is_first:span \
     -f 'tab' \
     -g $gold \
     $sys \
